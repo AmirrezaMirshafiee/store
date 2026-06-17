5de9cb8c1320fb45748bdab533d43a77e15e740f
Amirreza Mirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2026-06-17 17:39:38 +0330

    Update userCn.js

--- Modules/User/userCn.js
+++ Modules/User/userCn.js
@@ -1,6 +1,6 @@
 import User from "./userMd.js";
 import ApiFeatures, { HandleERROR, catchAsync } from "vanta-api";
-
+import bcryptjs from "bcryptjs";
 export const getAll = catchAsync(async (req, res, next) => {
   const { search = null } = req.query;
   const features = new ApiFeatures(User, req.query, req.role)
@@ -9,7 +9,7 @@ export const getAll = catchAsync(async (req, res, next) => {
         ? {
             phoneNumber: {
               $regex: search,
-              $option: "i",
+              $options: "i",
             },
           }
         : {},
@@ -49,23 +49,59 @@ export const getOne = catchAsync(async (req, res, next) => {
   return res.status(200).json(result);
 });
 export const update = catchAsync(async (req, res, next) => {
+  const { isActive = "null", role = null, fullName = null } = req.body;
   const { id } = req.params;
-  const { isActive = false, fullName = null, role = null } = req.body;
-  if (req?.role ==="admin" &&id?.toString() != req?.userId?.toString() &&(req.role === "admin" || req.role === "superAdmin")
-  ) {
-    return next(
-      new HandleERROR("you don't have permission to update account", 400),
-    );
+  if (req.role == "user" && id.toString() != req.userId.toString()) {
+    return next(new HandleERROR("you permission for update this account", 403));
   }
   const user = await User.findById(id);
-  if (req.role === "admin" || req.role === "superAdmin") {
-    user.isActive = isActive || user.isActive;
-    user.role = role || user.role;
+  if (
+    req.role == "admin" &&
+    id.toString() != req.userId.toString() &&
+    (user.role == "admin" || user.role == "superAdmin")
+  ) {
+    return next(new HandleERROR("you permission for update this account", 403));
   }
-  user.fullName=fullName ||user.fullName
-  const newUser= await user.save()
-  return res.status(201).json({
-    status:true,
-    data:newUser
-  })
+  if (req.role != "user") {
+    user.isActive = isActive == "null" ? user.isActive : isActive;
+  }
+  if (req.role == "superAdmin") {
+    user.role = role || user?.role;
+  }
+  user.fullName = fullName || user?.fullName;
+  const newUser = await user.save();
+  return res.status(200).json({
+    success: true,
+    message: "user Updated successfully",
+    data: newUser,
+  });
+});
+export const changePassword = catchAsync(async (req, res, next) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.userId);
+  if (!user) {
+    return next(new HandleERROR("user not found", 404));
+  }
+  if (!user.password) {
+    user.password = bcryptjs.hashSync(newPassword, 10);
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "password set successfully",
+    });
+  }
+  if (!oldPassword.trim() || !newPassword.trim()) {
+    return next(
+      new HandleERROR("oldPassword  and newPassword is required", 404),
+    );
+  }
+  const isMatch = bcryptjs.compareSync(oldPassword, user.password);
+  if (!isMatch) {
+    return next(new HandleERROR("old password not valid", 404));
+  }
+  user.password = bcryptjs.hashSync(newPassword, 10);
+  return res.status(200).json({
+    success: true,
+    message: "updated password  successfully",
+  });
 });
