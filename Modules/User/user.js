94e99e9561228963bb307fd5c46b351cda70e77b
Amirreza Mirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2026-06-17 17:39:35 +0330

    Update user.js

--- Modules/User/user.js
+++ Modules/User/user.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { getAll, getOne, update } from "./userCn.js";
+import { changePassword, getAll, getOne, update } from "./userCn.js";
 import isAdmin from "../../Middlewares/isAdmin.js";
-import { getAllUserValidator, updateUserValidator } from "./UserValidator.js";
+import {
+  changePasswordValidator,
+  getAllUserValidator,
+  updateUserValidator,
+  userIdParam,
+} from "./UserValidator.js";
 import { handleValidationErrors } from "../../Utils/handleValidationError.js";
 import isLogin from "../../Middlewares/isLogin.js";
 const userRouter = Router();
@@ -10,7 +15,15 @@ userRouter
   .route("/")
   .get(isAdmin, getAllUserValidator, handleValidationErrors, getAll);
 userRouter
+  .route("/change")
+  .patch(
+    isLogin,
+    changePasswordValidator,
+    handleValidationErrors,
+    changePassword,
+  );
+userRouter
   .route("/:id")
-  .get(isLogin, getOne)
+  .get(isLogin, userIdParam, handleValidationErrors, getOne)
   .patch(isLogin, updateUserValidator, handleValidationErrors, update);
 export default userRouter;
