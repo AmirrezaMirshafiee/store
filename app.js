25adf2ed99c3504fb1e62a28268aed7003c8d6f9
Amirreza Mirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2026-06-17 17:39:16 +0330

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import { fileURLToPath } from "url";
 import { catchError } from "vanta-api";
 import { exportValidationData } from "./Middlewares/ExportValidation.js";
-import { uploadRouter, userRouter } from "./Modules/index.js";
+import { authRouter, uploadRouter, userRouter } from "./Modules/index.js";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 export const __direname = path.dirname(__filename);
@@ -16,8 +16,9 @@ app.use(cors());
 app.use("/upload",express.static(`${__direname}/Public`));
 app.use(exportValidationData);
 
-app.use("api/upload",uploadRouter)
-app.use("api/users",userRouter)
+app.use("/api/upload",uploadRouter)
+app.use("/api/users",userRouter)
+app.use("/api/auth",authRouter)
 app.use((req, res, next) => {
   return res.status(404).json({
     success: false,
