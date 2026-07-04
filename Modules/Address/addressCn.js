13dec9ce0ef8af45df58d00ee1b5d365ae871cf9
Amirreza Mirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2026-07-04 17:18:54 +0330

    Update addressCn.js

--- Modules/Address/addressCn.js
+++ Modules/Address/addressCn.js
@@ -1,52 +1,100 @@
+import User from "../User/userMd.js";
 import Address from "./addressMd.js";
-import ApiFeatures, { HandleERROR, catchAsync } from "vanta-api";
-import { __direname } from "../../app.js";
+import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
+
 export const create = catchAsync(async (req, res, next) => {
-  const address = await Address.create(req.body);
+  const address = await Address.create({ ...req.body, userId: req.userId });
+  await User.findByIdAndUpdate(req.userId, {
+    $push: { addressId: address._id },
+  });
   return res.status(201).json({
     success: true,
-    message: "create address successfully",
+    message: "Address created successfully",
     data: address,
   });
 });
-
 export const getAll = catchAsync(async (req, res, next) => {
   const features = new ApiFeatures(Address, req.query, req.role)
-    .addManualFilters()
-    .sort()
-    .paginate()
-    .limitFields()
+    .addManualFilters(
+      req.role === "admin" || req.role === "superAdmin"
+        ? {}
+        : { userId: req.userId },
+    )
     .filter()
-    .populate()
-    .search();
-  const result = await features.execute();
-  return res.status(201).json(result);
+    .sort()
+    .limitFields()
+    .paginate()
+    .populate([{ path: "userId", select: "fullName phoneNumber" }])
+    .search(["province", "city", "title", "address", "NO", "floor"]);
+
+  const address = await features.execute();
+  return res.status(200).json(address);
 });
 export const getOne = catchAsync(async (req, res, next) => {
   const features = new ApiFeatures(Address, req.query, req.role)
     .addManualFilters(
       req.role === "admin" || req.role === "superAdmin"
         ? { _id: req.params.id }
-        : { _id: req.params.id },
+        : { $and: [{ userId: req.userId }, { _id: req.params.id }] },
     )
-    .sort()
-    .paginate()
-    .limitFields()
     .filter()
-    .populate([{ path: "userId" }])
-    .search();
-  const result = await features.execute();
-  return res.status(201).json(result);
-});
-export const update = catchAsync(async (req, res, next) => {
-  const address = await Address.findByIdAndUpdate(req.params.id, req.body, {
-    runValidators: true,
-    new: true,
-  });
-  return res.status(201).json({
-    success: true,
-    message: "update address successfully",
+    .sort()
+    .limitFields()
+    .paginate()
+    .populate([{ path: "userId", select: "fullName phoneNumber" }])
+    .search(["province", "city", "title", "address", "NO", "floor"]);
+
+  const address = await features.execute();
+  return res.status(200).json({
+    status: true,
+    message: "Address fetched successfully",
     data: address,
   });
 });
-export const remove = catchAsync(async (req, res, next) => {});
+export const update = catchAsync(async (req, res, next) => {
+  const { userId = null, ...othersData } = req.body;
+  const address = await Address.findById(req.params.id);
+  if (
+    address?.userId?.toString() !== req.userId?.toString() &&
+    req.role !== "admin" &&
+    req.role !== "superAdmin"
+  ) {
+    return next(
+      new HandleERROR("You are not authorized to update this address", 403),
+    );
+  }
+  const updatedAddress = await Address.findByIdAndUpdate(
+    req.params.id,
+    othersData,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  return res.status(200).json({
+    success: true,
+    message: "Address updated successfully",
+    data: updatedAddress,
+  });
+});
+export const remove = catchAsync(async (req, res, next) => {
+  const address = await Address.findById(req.params.id);
+  if (
+    address?.userId?.toString() !== req.userId?.toString() &&
+    req.role !== "admin" &&
+    req.role !== "superAdmin"
+  ) {
+    return next(
+      new HandleERROR("You are not authorized to delete this address", 403),
+    );
+  }
+  await Address.findByIdAndDelete(req.params.id);
+  await User.findByIdAndUpdate(address.userId, {
+    $pull: { addressId: req.params.id },
+  });
+  return res.status(200).json({
+    success: true,
+    message: "Address deleted successfully",
+  });
+});
