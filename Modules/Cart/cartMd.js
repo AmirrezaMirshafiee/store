615cdde4f65a3d18c47dd1f33389248b59b1200a
Amirreza Mirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2026-07-15 20:22:47 +0330

    Update cartMd.js

--- Modules/Cart/cartMd.js
+++ Modules/Cart/cartMd.js
@@ -1,10 +1,54 @@
 import mongoose from "mongoose";
+const itemsSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: [true, "productId is required"],
+    },
+    productVariantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProductVariant",
+      required: [true, "productVariantId is required"],
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Category is required"],
+    },
+    brandId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: [true, "brandId is required"],
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+      min: [1, "minimum 1"],
+    },
+  },
+  { _id: false },
+);
 const cartSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: [true, "userId is required"],
+    },
+    items: {
+      type: [itemsSchema],
+      default: [],
+    },
+    totalPrice: {
+      type: Number,
+      default: 0,
+      min: [0, "minimum 0"],
+    },
+    totalPriceAfterDiscount: {
+      type: Number,
+      default: 0,
+      min: [0, "minimum 0"],
     },
   },
   { timestamps: true, versionKey: false },
