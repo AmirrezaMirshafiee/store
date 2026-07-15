d8ebc53926af7b395fb23bedd82f8ba220fba9dd
Amirreza Mirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2026-07-15 20:23:03 +0330

    Update index.js

--- Modules/index.js
+++ Modules/index.js
@@ -9,4 +9,5 @@ import productRouter from "./Product/product.js";
 import productVariantRouter from "./ProductVariant/productVariant.js";
 import addressRouter from "./Address/address.js";
 import commentRouter from "./Comment/comment.js";
-export {uploadRouter,userRouter,authRouter,brandRouter,sliderRouter,categoryRouter,variantRouter,productRouter,productVariantRouter,addressRouter,commentRouter};
+import cartRouter from "./Cart/cart.js";
+export {uploadRouter,userRouter,authRouter,brandRouter,sliderRouter,categoryRouter,variantRouter,productRouter,productVariantRouter,addressRouter,commentRouter,cartRouter};
